2e4ae5a65e762e40ca4d7731b06092f97043264e
Stéphane Ngoyi <sngoyi5@gmail.com>
2024-01-06 00:13:05 +0100

    updating

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,6 @@ import router from "./router";
 import { store } from "../store";
 import { ReactFlowProvider } from "reactflow";
 
-import { registerSW } from "virtual:pwa-register";
-
-// add this to prompt for a refresh
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm("New content available. Reload?")) {
-      updateSW(true);
-    }
-  },
-});
-
 let rootElement: any = document.getElementById("root");
 
 ReactDOM.createRoot(rootElement).render(
